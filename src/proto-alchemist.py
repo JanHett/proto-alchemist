d39f2f2dcefd1b4991d2db19c53bdf9906c8230a
Jan Hettenkofer <jan@hettenkofer.net>
2019-06-06 00:16:39 +0200

    Massive performance improvements by using cv2's math instead of custom logic
    Also fixed a number of edge-case bugs

--- src/proto-alchemist.py
+++ src/proto-alchemist.py
@@ -32,72 +32,29 @@ def find_base(neg, print_progress=False):
         neg.item(white_sample[0], white_sample[1], 2)
     ]
 
-def average_base(base, print_progress=False):
-    avg = [0 for chan in range(base.shape[2])]
-    samples = 0
-
-    for y in range(base.shape[1]):
-        if print_progress:
-            progress = y/base.shape[1] * 100
-            if progress.is_integer():
-                print(
-                    ERASE_LINE + f'Averaging base... {progress} %',
-                    end='\r'),
-        for x in range(base.shape[0]):
-            samples += 1
-            for chan in range(neg.shape[2]):
-                avg[chan] += base.item(x, y, chan)
-
-    for chan_avg in avg:
-        chan_avg = chan_avg / samples
-
-    return avg
-
 def invert(neg, base, print_progress=False):
-    scale_b = 1 / base[0]
-    scale_g = 1 / base[1]
-    scale_r = 1 / base[2]
+    if print_progress:
+            print(
+                ERASE_LINE + 'Removing orange mask...', end='\r'),
 
     # remove orange mask
     b,g,r = cv2.split(neg)
-    b = b * scale_b
-    g = g * scale_g
-    r = r * scale_r
+    b = b * (1 / base[0])
+    g = g * (1 / base[1])
+    r = r * (1 / base[2])
     res = cv2.merge((b,g,r))
-
-    # invert
-    # K = 0.00005
-    # gamP = 2.2
-    # gamC = 0.45
-    # res = ((K / (res**gamP))**gamC)
 
     if print_progress:
             print(
                 ERASE_LINE + 'Inverting...', end='\r'),
+
+    # invert
     res = 1 - res
 
-    brightest = [0, 0, 0]
-
-    for y in range(res.shape[1]):
-        if print_progress:
-            progress = y/res.shape[1] * 100
-            if progress.is_integer():
-                print(
-                    ERASE_LINE + f'Scaling... {progress} %', end='\r'),
-        for x in range(res.shape[0]):
-            for chan in range(res.shape[2]):
-                if res.item(x, y, chan) > res.item(
-                    brightest[0],
-                    brightest[1],
-                    brightest[2]
-                ):
-                    brightest = [x, y, chan]
-
-    res = res * (1/res.item(
-                    brightest[0],
-                    brightest[1],
-                    brightest[2]
-                ))
+    if print_progress:
+            print(
+                ERASE_LINE + 'Normalizing...', end='\r'),
+    res = cv2.normalize(res, None, 0.0, 1.0, cv2.NORM_MINMAX)
 
     return res
 
@@ -108,7 +65,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Convert a color negative image to a positive.')
 
-    parser.add_argument('negative',
+    parser.add_argument('sources',
+        nargs='+',
         help='The linear scan of the film or a glob pattern describing '
         'multiple files to treat')
     
@@ -119,7 +77,7 @@ if __name__ == '__main__':
         'neutral base. Omitting this will cause Spice/Alchemist to search for '
         'the lightest pixel in the negative and assume that it is the base '
         'colour.')
-    
+
     parser.add_argument('--show', '-s',
         action='store_true',
         help='Show the converted image.')
@@ -128,21 +86,11 @@ if __name__ == '__main__':
         help='Save the converted image to this directory using the name of the '
         'input file. Either this or --show has to be specified.')
     
-    # parser.add_argument('--color-space', '-c',
-    #     default='ProphotoRGB',
-    #     choices=[
-    #         'ProphotoRGB',
-    #         'AdobeRGB',
-    #         'sRGB'
-    #         ],
-    #     help='[ UNIMPLEMENTED ] The colour space to save the result with. '
-    #     'Defaults to "ProphotoRGB".')
-    
     parser.add_argument('--bit-depth', '-d',
         type=int,
         default=16,
         choices=[ 8, 16, 32 ],
-        help='[ UNIMPLEMENTED ] The bit depth to save the result with. '
+        help='The bit depth to save the result with. '
         'Defaults to 16 bits per channel.')
 
     args = parser.parse_args()
@@ -153,21 +101,28 @@ if __name__ == '__main__':
         print('Either --show or --output has to be specified.')
         sys.exit(-1)
 
-    # if not os.path.isfile(args.negative):
-    #     print(f'"{args.negative}" is not a file.')
-    #     sys.exit(-1)
-
     # Do the actual processing
 
-    files = glob.glob(args.negative)
+    # Find the mean of the provided orange mask
+    if args.base:
+        base = cv2.imread(args.base, cv2.IMREAD_UNCHANGED)
+        base = np.float32(base) / np.iinfo(base.dtype).max
+        base = cv2.mean(base)
+        print(f"Found base: {base}")
+
+    if len(args.sources) == 1:
+        files = glob.glob(args.sources[0])
+    else:
+        files = args.sources
     counter = 0
     for file in files:
         counter += 1
         print(f'Processing file {counter} of {len(files)}...')
-        neg = cv2.imread(file)
-        if args.base:
-            base = average_base(cv2.imread(args.base), True)
-        else:
+        neg = cv2.imread(file, cv2.IMREAD_UNCHANGED).astype('float')
+
+        # fall back on searching for maximum pixel value in case of unspecified
+        # base colour
+        if not args.base:
             base = find_base(neg, True)
 
         positive = invert(neg, base, True)
@@ -176,9 +131,19 @@ if __name__ == '__main__':
             filename = os.path.join(args.output, os.path.basename(file))
             os.makedirs(os.path.dirname(os.path.abspath(filename)),
                 exist_ok=True)
+
+            if args.bit_depth == 8:
+                d_type = np.uint8
+                maxval = np.iinfo(d_type).max
+            elif args.bit_depth == 16:
+                d_type = np.uint16
+                maxval = np.iinfo(d_type).max
+            elif args.bit_depth == 32:
+                d_type = np.float32
+                maxval = 1
             cv2.imwrite(
                 filename,
-                (np.clip(positive, 0, 1) * 65535).astype(np.uint16))
+                (np.clip(positive, 0, 1) * maxval).astype(d_type))
 
         if args.show:
             cv2.imshow(
